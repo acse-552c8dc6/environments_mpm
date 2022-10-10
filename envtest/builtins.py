70fbdb3210f4e64953f7226e8e9dbdaddece5134
Haili Yuan <haili.yuan22@ic.ac.uk>
2022-10-10 10:52:01 +0100

    final

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
 
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'my_concat']
 
 
 def rand_array(shape):
@@ -15,3 +16,9 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def my_concat(A, B, C=None):
+    try:
+        return pd.concat([A, B, C])
+    except TypeError:
+        return pd.concat([A, B])
